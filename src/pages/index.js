7edad6361f7cb53e1c0e459049a7153cab352709
Nikita Chursin <chursinn91@gmail.com>
2020-04-29 10:31:20 +0300

    removed link to the other page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,6 @@ const IndexPage = () => (
 
     <Calendar/>
 
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div> */}
-    <Link to="/page-2/">Go to page 2</Link>
-
-
   </Layout>
 )
 
